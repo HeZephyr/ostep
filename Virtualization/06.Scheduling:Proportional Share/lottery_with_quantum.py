81fbe6913ef2152f6057cb8ee0a70c6fc375f6f4
zfhe <2825841950@qq.com>
2024-03-28 20:46:07 +0800

    finish the homework of Scheduling:Proportional Share

---
+++ Virtualization/06.Scheduling:Proportional Share/lottery_with_quantum.py
@@ -0,0 +1,67 @@
+#! /usr/bin/env python3
+
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+uList = [0] * 100
+
+for i in range(30):
+    random.seed(i)
+
+    for quantum in range(1, 101):
+        tickTotal = 200
+        runTotal = 200
+        jobs = 2
+        jobList = [[0, 100, 100], [1, 100, 100]]
+
+        clock = 0
+        finishTime = []
+        # run jobs
+        for k in range(runTotal):
+            r = int(random.random() * 1000001)
+            winner = int(r % tickTotal)
+
+            current = 0
+            wjob = 0
+            wrun = 0
+            wtix = 0
+            for (job, runtime, tickets) in jobList:
+                current += tickets
+                if current > winner and runtime > 0:
+                    (wjob, wrun, wtix) = (job, runtime, tickets)
+                    break
+
+            # now do the accounting
+            if wrun >= quantum:
+                wrun -= quantum
+            else:
+                wrun = 0
+
+            jobList[wjob] = (wjob, wrun, wtix)
+
+            clock += quantum
+
+            # job completed!
+            if wrun == 0:
+                tickTotal -= wtix
+                jobs -= 1
+                finishTime.append(clock)
+
+            if jobs == 0:
+                break
+
+        u = round(finishTime[0] / finishTime[1], 2)
+        uList[quantum - 1] += u
+
+fig = plt.figure()
+x = np.linspace(1, 100, 100)
+plt.plot(x, [u / 30 for u in uList], color='orange')
+plt.ylim(0, 1)
+plt.margins(0)
+plt.xlabel('Quantum Size')
+plt.ylabel('Unfairness (Average)')
+plt.title('Lottery Fairness Study')
+plt.savefig('lottery_with_quantum.png', dpi=227)
+plt.show()
---
+++ Virtualization/06.Scheduling:Proportional Share/stride_with_job_length.py
@@ -0,0 +1,65 @@
+#! /usr/bin/env python3
+
+import random
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+uList = [0] * 1000
+
+for i in range(30):
+    random.seed(i)
+
+    for jobLength in range(1, 1001):
+        runTotal = 2 * jobLength
+        jobs = 2
+        quantum = 1
+        jobList = [[0, jobLength, 0], [1, jobLength, 0]]
+        clock = 0
+        finishTime = []
+        r = int(random.random() * 1000001)
+        stride = int(r % 100)
+        # run jobs
+        for k in range(runTotal):
+            wjob = 0
+            wrun = 0
+            wpass = 0
+            if (jobList[0][2] <= jobList[1][2]):
+                # run job 0
+                (wjob, wrun, wpass) = jobList[0]
+            else:
+                # run job 1
+                (wjob, wrun, wpass) = jobList[1]
+
+            # now do the accounting
+            if wrun >= quantum:
+                wrun -= quantum
+            else:
+                wrun = 0
+
+            wpass += stride
+            jobList[wjob] = (wjob, wrun, wpass)
+
+            clock += quantum
+
+            # job completed!
+            if wrun == 0:
+                jobs -= 1
+                finishTime.append(clock)
+
+            if jobs == 0:
+                break
+
+        u = round(finishTime[0] / finishTime[1], 2)
+        uList[jobLength - 1] += u
+
+fig = plt.figure()
+x = np.linspace(1, 1000, 1000)
+plt.plot(x, [u / 30 for u in uList], color='orange')
+plt.ylim(0, 1)
+plt.margins(0)
+plt.xlabel('Job Length')
+plt.ylabel('Unfairness (Average)')
+plt.title('Stride Scheduling')
+plt.savefig('stride_with_quantum.png', dpi=227)
+plt.show()

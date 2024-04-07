6b437dac30c60113250752c7d019268ead93f6d1
zfhe <2825841950@qq.com>
2024-04-07 14:33:46 +0800

    update the common_threads.h

--- Concurrency/include/common_threads.h
+++ Concurrency/include/common_threads.h
@@ -12,6 +12,9 @@
 #define Pthread_create(thread, attr, start_routine, arg) assert(pthread_create(thread, attr, start_routine, arg) == 0);
 #define Pthread_join(thread, value_ptr)                  assert(pthread_join(thread, value_ptr) == 0);
 
+#define Pthread_mutex_init(m, v)                         assert(pthread_mutex_init(m, v) == 0);
+#define Pthread_cond_init(cond, v)                       assert(pthread_cond_init(cond, v) == 0);
+
 #define Pthread_mutex_lock(m)                            assert(pthread_mutex_lock(m) == 0);
 #define Pthread_mutex_unlock(m)                          assert(pthread_mutex_unlock(m) == 0);
 #define Pthread_cond_signal(cond)                        assert(pthread_cond_signal(cond) == 0);

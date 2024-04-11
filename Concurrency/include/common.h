048b69784c14033394e361acc2bfb010a8b157f9
zfhe <2825841950@qq.com>
2024-04-11 21:07:53 +0800

    updated common.h

--- Concurrency/include/common.h
+++ Concurrency/include/common.h
@@ -3,6 +3,7 @@
 
 #include <sys/time.h>
 #include <sys/stat.h>
+#include <stdlib.h>
 #include <assert.h>
 
 double GetTime() {
@@ -17,5 +18,9 @@ void Spin(int howlong) {
     while ((GetTime() - t) < (double) howlong)
 	; // do nothing in loop
 }
+
+#define Malloc(s) ({ void *p = malloc(s); assert(p != NULL); p; })
+#define Time_GetSeconds() ({ struct timeval t; int rc = gettimeofday(&t, NULL); assert(rc == 0); (double) t.tv_sec + (double) t.tv_usec/1e6; })
+
 
 #endif // __common_h__
